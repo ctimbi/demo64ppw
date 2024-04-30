1c71fbbdd88143ce4085ca2d727c61a935c769d6
Cristian <ctimbi@gmail.com>
2024-04-30 07:58:30 -0500

    Service

--- src/app/pages/acercade/acercade.component.ts
+++ src/app/pages/acercade/acercade.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ContactosService } from '../../services/contactos.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-acercade',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './acercade.component.html',
   styleUrl: './acercade.component.scss'
 })
 export class AcercadeComponent {
 
+  msg: string = 'nuevo'
+
+  messages: string[] = []
+
+  constructor(private contactosService: ContactosService){
+
+  }
+
+  ngOnInit(){
+    this.messages = this.contactosService.get()
+  }
+
+  agregar(){
+    this.contactosService.add(this.msg)
+  }
+
+  limpiar(){
+    this.contactosService.clear()
+    this.ngOnInit()
+  }
 }
---
+++ src/app/services/contactos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactosService {
+
+  mensajes: string[] = ['Hola', 'Buenos días', 'Saludos', 'Cuenca']
+
+  constructor() { }
+
+  add(message: string){
+    this.mensajes.push(message)
+  }
+
+  clear(){
+    this.mensajes = []
+  }
+
+  get(){
+    return this.mensajes
+  }
+}
